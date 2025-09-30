d50cbbf194c0891470e38113de973ca2cdd90a32
nikhilyadavcode <147080866+nikhilyadavcode@users.noreply.github.com>
2025-09-30 09:03:44 +0530

    python practise

--- Hello.py
+++ Hello.py
@@ -1,3 +1,18 @@
-print("Hello World")
+# print("Hello World")
 
-print("Welcome to python Programming")
+# print("Welcome to python Programming")
+
+# print("Hello I am Nikhil yadav")
+# print("I am learning python")
+
+
+name="Ajay"
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
--- Sum.py
+++ Sum.py
@@ -1,12 +1,4 @@
-def add (a, b):
-    return a+b
-
-
-a=int(input("Enter the first number:"))
-b=int(input("Enter the second number:"))
-
-result=add(a,b)
-print("The sum is:",result)
-
-
-
+import camelcase
+c=camelcase.CamelCase()
+text="Hello I am Nikhil"
+print(c.hump(text))
